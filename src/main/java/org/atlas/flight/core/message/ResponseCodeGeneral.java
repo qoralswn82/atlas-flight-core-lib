a0d4f8be6857e8aceab0fcb714e93e39b76fde44
백민주 <mj.baek@okestro.com>
2026-04-13 17:36:30 +0900

    fix(ATLAS-54) : 에러 응답코드 접두사 변경

--- src/main/java/org/atlas/flight/core/message/ResponseCodeGeneral.java
+++ src/main/java/org/atlas/flight/core/message/ResponseCodeGeneral.java
@@ -6,13 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ResponseCodeGeneral implements ResponseCodeInterface {
-	SUCCESS("BPLTE200", "정상 처리되었습니다.", ""),
-	BAD_REQUEST("BPLTE400", "잘못된 요청입니다.", ""),
-	NOT_FOUND("BPLTE404", "요청한 리소스를 찾을 수 없습니다.", ""),
-	FORBIDDEN("BPLTE403", "접근 권한이 없습니다.", "해당 리소스에 대한 권한이 없습니다."),
-	UNAUTHORIZED("BPLTE401", "인증이 필요합니다.", "로그인 후 다시 시도해주세요."),
-	TOO_MANY_REQUESTS("BPLTE429", "요청이 너무 많습니다.", "10분당 호출 한도를 초과했습니다. 잠시 후 다시 시도해주세요."),
-	UNKNOWN("BPLTE500", "알 수 없는 오류가 발생했습니다.", "");
+	SUCCESS("ATF200", "정상 처리되었습니다.", ""),
+	BAD_REQUEST("ATF400", "잘못된 요청입니다.", ""),
+	NOT_FOUND("ATF404", "요청한 리소스를 찾을 수 없습니다.", ""),
+	FORBIDDEN("ATF403", "접근 권한이 없습니다.", "해당 리소스에 대한 권한이 없습니다."),
+	UNAUTHORIZED("ATF401", "인증이 필요합니다.", "로그인 후 다시 시도해주세요."),
+	TOO_MANY_REQUESTS("ATF429", "요청이 너무 많습니다.", "10분당 호출 한도를 초과했습니다. 잠시 후 다시 시도해주세요."),
+	UNKNOWN("ATF500", "알 수 없는 오류가 발생했습니다.", "");
 	
 	private final String resultCode;
 	private final String resultMessage;
